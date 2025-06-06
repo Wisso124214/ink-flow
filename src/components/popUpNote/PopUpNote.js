463605878bab5659304752ad1de4526bd672aef1
Wisso124214 <luisdavidbustosnunez@gmail.com>
2025-06-06 04:30:09 -0400

    fix styles and add functions to style text on popUpNote

--- src/components/popUpNote/PopUpNote.js
+++ src/components/popUpNote/PopUpNote.js
@@ -3,12 +3,15 @@ import './PopUpNote.css';
 
 export default function PopUpNote({ showPopUp, setShowPopUp, selectedNote, hyphenText }) {
   
+  const isLeavingStartTags = false;
+
   const maxCharacters = 1000;
   const charsToHypenTitle = 13;
   const charsToHypenText = 29;
 
   const [characterCount, setCharacterCount] = React.useState(0);
   const [isEditing, setIsEditing] = React.useState(false);
+  const [isTextAreaContentFocused, setIsTextAreaContentFocused] = React.useState(false);
   const [focus, setFocus] = React.useState('content-text-textarea');
   const scrollYContent = React.useRef(0);
 
@@ -21,15 +24,15 @@ export default function PopUpNote({ showPopUp, setShowPopUp, selectedNote, hyphe
     e.stopPropagation();
     
     const editButtons = document.getElementById('edit-text-buttons');
-
+    
     if (editButtons) {
-      if (e.target.classList.contains('content-title') && isEditing) {
+      if (e.target.classList.contains('content-text') && isEditing) {
         for (let children of editButtons.children) {
-          children.classList.add('disabled');
+          children.classList.remove('disabled');
         }
       } else {
         for (let children of editButtons.children) {
-          children.classList.remove('disabled');
+          children.classList.add('disabled');
         }
       }
     }
@@ -37,7 +40,191 @@ export default function PopUpNote({ showPopUp, setShowPopUp, selectedNote, hyphe
 
   const handleClickEditButton = (e) => {
     e.stopPropagation();
+
+    if (e.target.classList.contains('disabled') || !isTextAreaContentFocused)
+      return;
+
+    const textAreaContent = document.getElementById('content-text-textarea');
+    textAreaContent.focus();
+
+    /**
+     * - Recorrer el texto en busca de tags que coincidan con el botón pulsado (LIFO)
+        * - Si se encuentra un startTag sin un endTag, se deja como está.
+        * - Si se encuentra un endTag sin un startTag, se elimina.
+     * - Guardar en un arreglo de objetos los tags encontrados y su posición.
+     * - Comprobar que el texto seleccionado coincide con un tag.
+     * - Si coincide uno de los dos puntos seleccionados dentro de un tag, eliminarlo.
+     * - Si no coincide, añadir el tag al texto en las posiciones seleccionadas.
+     */
+
+    const extractTags = (text) => {
+      let tags = [];
+
+      const regex = new RegExp(`(<${tagName}>|</${tagName}>)`, 'g');
+      let match;
+      while ((match = regex.exec(text)) !== null) {
+        tags.push({
+          isStartTag: new RegExp(`(<${tagName}>)`).exec(match[0]) ? true : false,
+          position: match.index,
+          tag: match[0],
+        });
+      }
+      return tags;
+    }
+
+    const pairTags = (tags, lifo = [], result = []) => {
+      if (tags.length === 0) {
+        if (lifo.length > 0 && isLeavingStartTags) {
+          // If there are unmatched start tags, add them to the result
+          lifo.forEach(tag => {
+            result.unshift({
+              start: tag.position,
+              end: null, // No end tag for unmatched start tags
+            });
+          });
+        }
+        return result;
+      }
+
+      /**
+       * - Si es un startTag, se añade al lifo
+       * - Si es un endTag, se comprueba si hay un startTag en el lifo
+       *  - Si hay un startTag, se emparejan y se añaden al resultado
+       *  - Si hay más de un startTag, se empareja con el último startTag del lifo
+       *  - Si no hay un startTag, se elimina el endTag
+       */
+
+      const currentTag = tags.shift();
+      if (currentTag.isStartTag) {
+        lifo.unshift(currentTag);
+      } else {
+        const startTagIndex = lifo.findIndex(tag => tag.isStartTag && tag.position < currentTag.position);
+        if (startTagIndex !== -1) {
+          const startTag = lifo.splice(startTagIndex, 1)[0];
+          result.unshift({
+            start: startTag.position,
+            end: currentTag.position,
+          });
+        }
+      }
+      return pairTags(tags, lifo, result);
+    }
+
+    const toggleTag = (pairedTags, selection) => {
+      const newPairedTags = [...pairedTags];
+      let indexSelectionMatchTag = -1;
+
+      newPairedTags.forEach((tag, index) => {
+        if ((tag.start <= selection.start && tag.end >= selection.start) || (tag.start <= selection.end && tag.end >= selection.end)) {
+          indexSelectionMatchTag = index;
+        }
+      });
+
+      let extraSpace = 0;
+
+      // To select minimum one character when the selection is empty
+      if (selection.start === selection.end) 
+        extraSpace = 1;
+
+      if (indexSelectionMatchTag === -1 && tagName) {
+        // If no matching tag is found, create a new one
+        newPairedTags.push({
+          start: selection.start,
+          end: selection.end + tagName.length + 2 + extraSpace, 
+          isNewTag: true,
+        });
+      } else {
+        // If a matching tag is found, remove it
+        newPairedTags.splice(indexSelectionMatchTag, 1);
+      }
+      return newPairedTags;
+    }
     
+    // Delete the tags from the textContent
+    const deleteTagsFromText = (text) => {
+      const regex = new RegExp(`(<${tagName}>|</${tagName}>)`);
+      let match;
+
+      while ((match = regex.exec(text)) !== null) {
+        text = text.slice(0, match.index) + text.slice(match.index + match[0].length);
+      }
+      return text;
+    }
+
+    // Desestructurar pairedTags en un arreglo de objetos que tengan position y tag
+    // Ordenar el arreglo de objetos por position
+    const deconstructTags = (tags) => (
+       tags.map(tag => (
+        [{
+          position: tag.start,
+          tag: `<${tagName}>`,
+          isNewTag: tag.isNewTag || false,
+        }, {
+          position: tag.end,
+          tag: `</${tagName}>`,
+          isNewTag: tag.isNewTag || false,
+        }]
+      ))
+      .flat()
+      .sort((a, b) => a.position - b.position)
+      .filter(tag => tag.position !== null)
+    )    
+    
+    // Add the new tags to the textContent
+    const addNewTagsToText = (text, tags) => {
+      // Recorrer el texto y añadir los tags en las posiciones correspondientes (en un for que añada de a un tag por iteración)
+        // Se debe tomar en cuenta isNewTag para correr las posiciones y añadir el tag en la posición correcta
+
+      if (text[text.length - 1] !== ' ')
+        text += ' '; 
+
+      let newText = '';
+      let offset = 0;
+
+      text.split('').forEach((char, index) => {
+        const tag = tags.filter(tag => tag.position === index + offset);
+        // const tag = tags.filter(tag => tag.position === index);
+
+        let textToAdd = '';
+
+        if (tag.length > 0) {
+          for (let t of tag) {
+            if (!t.tag.includes('undefined')) {
+              offset += t.tag.length;
+              textToAdd += t.tag;
+            }
+          }
+        }
+        
+        textToAdd += char;
+        newText += textToAdd;
+      })
+      return newText;
+    }
+
+    const tagName = e.target.classList.value.split('')[0];
+    const textContent = textAreaContent.value;
+
+    const selection = {
+      start: textAreaContent.selectionStart,
+      end: textAreaContent.selectionEnd,
+    };
+
+    const tags = extractTags(textContent);
+    const pairedTags = pairTags(tags);
+
+    const updatedTags = toggleTag(pairedTags, selection);
+    const textWithoutTags = deleteTagsFromText(textContent);
+    const arrTags = deconstructTags(updatedTags);
+    
+    let newTextWithTags = addNewTagsToText(textWithoutTags, arrTags);
+
+    textAreaContent.value = newTextWithTags;
+    textAreaContent.selectionStart = selection.start;
+    textAreaContent.selectionEnd = selection.end;
+
+    setCharacterCount(newTextWithTags.length);
+    selectedNote.content = newTextWithTags;
   }
 
   React.useEffect(() => {
@@ -66,31 +253,32 @@ export default function PopUpNote({ showPopUp, setShowPopUp, selectedNote, hyphe
           { isEditing ? 
             <>
               <textarea 
-              id='content-title-textarea'
-              className='content-title'
-              placeholder='Title of your note...'
-              defaultValue={selectedNote.title}
-              maxLength={100}
-              onChange={(e)=>{
-                selectedNote.title = e.target.value;
-              }} 
-              />
-            <textarea
-              id='content-text-textarea'
-              className='content-text' 
-              placeholder='Write your note here...'
-              defaultValue={selectedNote.content}
-              onChange={(e)=>{
-                setCharacterCount(e.target.value.length);
-                selectedNote.content = e.target.value;
-              }} 
-              onScroll={(e) => {
-                if (e.target.scrollTop !== scrollYContent.current) {
-                  scrollYContent.current = e.target.scrollTop;
-                }
-              }}
-              maxLength={maxCharacters}
-              />
+                id='content-title-textarea'
+                className='content-title'
+                placeholder='Title of your note...'
+                defaultValue={selectedNote.title}
+                maxLength={100}
+                onChange={(e)=>{
+                  selectedNote.title = e.target.value;
+                }} 
+                />
+              <textarea
+                id='content-text-textarea'
+                className='content-text' 
+                placeholder='Write your note here...'
+                defaultValue={selectedNote.content}
+                onClick={() => setIsTextAreaContentFocused(true)}
+                onChange={(e)=>{
+                  setCharacterCount(e.target.value.length);
+                  selectedNote.content = e.target.value;
+                }} 
+                onScroll={(e) => {
+                  if (e.target.scrollTop !== scrollYContent.current) {
+                    scrollYContent.current = e.target.scrollTop;
+                  }
+                }}
+                maxLength={maxCharacters}
+                />
 
               <div id='edit-text-buttons' className='edit-text-buttons'>
                 <svg onClick={handleClickEditButton} className='bold' width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.10505 12C4.70805 12 4.4236 11.912 4.25171 11.736C4.0839 11.5559 4 11.2715 4 10.8827V4.11733C4 3.72033 4.08595 3.43588 4.25784 3.26398C4.43383 3.08799 4.71623 3 5.10505 3C6.42741 3 8.25591 3 9.02852 3C10.1373 3 11.0539 3.98153 11.0539 5.1846C11.0539 6.08501 10.6037 6.81855 9.70327 7.23602C10.8657 7.44851 11.5176 8.62787 11.5176 9.48128C11.5176 10.5125 10.9902 12 9.27734 12C8.77742 12 6.42626 12 5.10505 12ZM8.37891 8.00341H5.8V10.631H8.37891C8.9 10.631 9.6296 10.1211 9.6296 9.29877C9.6296 8.47643 8.9 8.00341 8.37891 8.00341ZM5.8 4.36903V6.69577H8.17969C8.53906 6.69577 9.27734 6.35939 9.27734 5.50002C9.27734 4.64064 8.48047 4.36903 8.17969 4.36903H5.8Z" fill="currentColor"></path></svg>
@@ -98,8 +286,6 @@ export default function PopUpNote({ showPopUp, setShowPopUp, selectedNote, hyphe
                 <svg onClick={handleClickEditButton} className='underscore' width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.00001 2.75C5.00001 2.47386 4.77615 2.25 4.50001 2.25C4.22387 2.25 4.00001 2.47386 4.00001 2.75V8.05C4.00001 9.983 5.56702 11.55 7.50001 11.55C9.43301 11.55 11 9.983 11 8.05V2.75C11 2.47386 10.7762 2.25 10.5 2.25C10.2239 2.25 10 2.47386 10 2.75V8.05C10 9.43071 8.88072 10.55 7.50001 10.55C6.1193 10.55 5.00001 9.43071 5.00001 8.05V2.75ZM3.49998 13.1001C3.27906 13.1001 3.09998 13.2791 3.09998 13.5001C3.09998 13.721 3.27906 13.9001 3.49998 13.9001H11.5C11.7209 13.9001 11.9 13.721 11.9 13.5001C11.9 13.2791 11.7209 13.1001 11.5 13.1001H3.49998Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
                 <svg onClick={handleClickEditButton} className='strikethrough' width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.00003 3.25C5.00003 2.97386 4.77617 2.75 4.50003 2.75C4.22389 2.75 4.00003 2.97386 4.00003 3.25V7.10003H2.49998C2.27906 7.10003 2.09998 7.27912 2.09998 7.50003C2.09998 7.72094 2.27906 7.90003 2.49998 7.90003H4.00003V8.55C4.00003 10.483 5.56703 12.05 7.50003 12.05C9.43303 12.05 11 10.483 11 8.55V7.90003H12.5C12.7209 7.90003 12.9 7.72094 12.9 7.50003C12.9 7.27912 12.7209 7.10003 12.5 7.10003H11V3.25C11 2.97386 10.7762 2.75 10.5 2.75C10.2239 2.75 10 2.97386 10 3.25V7.10003H5.00003V3.25ZM5.00003 7.90003V8.55C5.00003 9.93071 6.11932 11.05 7.50003 11.05C8.88074 11.05 10 9.93071 10 8.55V7.90003H5.00003Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
                 <svg onClick={handleClickEditButton} className='overline' width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.49985 1.10001C3.27894 1.10001 3.09985 1.27909 3.09985 1.50001C3.09985 1.72092 3.27894 1.90001 3.49985 1.90001H11.4999C11.7208 1.90001 11.8999 1.72092 11.8999 1.50001C11.8999 1.27909 11.7208 1.10001 11.4999 1.10001H3.49985ZM4.99995 4.25001C4.99995 3.97387 4.77609 3.75001 4.49995 3.75001C4.22381 3.75001 3.99995 3.97387 3.99995 4.25001V9.55001C3.99995 11.483 5.56695 13.05 7.49995 13.05C9.43295 13.05 11 11.483 11 9.55001V4.25001C11 3.97387 10.7761 3.75001 10.5 3.75001C10.2238 3.75001 9.99995 3.97387 9.99995 4.25001V9.55001C9.99995 10.9307 8.88066 12.05 7.49995 12.05C6.11924 12.05 4.99995 10.9307 4.99995 9.55001V4.25001Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                <svg onClick={handleClickEditButton} className='tabulation' width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.95 1.50005C12.95 1.25152 12.7485 1.05005 12.5 1.05005C12.2514 1.05005 12.05 1.25152 12.05 1.50005L12.05 13.5C12.05 13.7486 12.2514 13.95 12.5 13.95C12.7485 13.95 12.95 13.7486 12.95 13.5L12.95 1.50005ZM6.5683 3.93188C6.39257 3.75614 6.10764 3.75614 5.93191 3.93188C5.75617 4.10761 5.75617 4.39254 5.93191 4.56827L8.41371 7.05007L0.499984 7.05007C0.251456 7.05007 0.0499847 7.25155 0.0499847 7.50007C0.0499846 7.7486 0.251457 7.95007 0.499984 7.95007L8.41371 7.95007L5.93191 10.4319C5.75617 10.6076 5.75617 10.8925 5.93191 11.0683C6.10764 11.244 6.39257 11.244 6.56831 11.0683L9.8183 7.81827C9.99404 7.64254 9.99404 7.35761 9.8183 7.18188L6.5683 3.93188Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                {/* <svg onClick={handleClickEditButton} className='linebreak' fill="currentColor" width="15" height="15" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" > <path d="M20.586 14.414L24.172 18H6V8H4v10a2.002 2.002 0 002 2h18.172l-3.586 3.586L22 25l6-6-6-6z" /> <path data-name="&lt;Transparent Rectangle&gt;" fill="none" d="M0 0H32V32H0z" /></svg> */}
               </div>
             </>
            : <>
